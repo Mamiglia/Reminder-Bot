72c91362b3ad0350b3bcf38dfc7971bfdfa51049
Mamiglia <matteo.migliarini.01@gmail.com>
2018-06-21 14:18:37 +0200

    fixes and improvements
    added a pretty indentation when you have to confrom something
    check if a remind is effectively canceled
    check if the user has set a timezone
    delete old buttons

--- Reminderbot.py
+++ Reminderbot.py
@@ -1,7 +1,6 @@
 import botogram
 import sqlite3
-from datetime import datetime
-from datetime import timedelta
+from datetime import datetime, timedelta
 import redis
 import json
 from dateutil import parser
@@ -9,23 +8,23 @@ dat = sqlite3.connect("dat.db")
 d = dat.cursor()
 # d.execute('DROP TABLE IF EXISTS users')
 # d.execute('DROP TABLE IF EXISTS remind')
-d.execute('CREATE TABLE IF NOT EXISTS users (userid INTEGER PRIMARY KEY, timezone INTEGER, DST INTEGER)')
+d.execute('CREATE TABLE IF NOT EXISTS users (userid INTEGER PRIMARY KEY, timezone INTEGER, DST INTEGER DEFAULT 0)')
 d.execute("CREATE TABLE IF NOT EXISTS remind (userid INTEGER, mesid INTEGER, tim DATE)")
 dat.commit()
 bot = botogram.create('')
-bot.about = "I AM THE GREAT REMINDERMASTER, with my 512Kb of memory I can Remember ANYTHING you desire, I am at your service, just push /remind"
+bot.about = "I AM THE GREAT REMINDERUS, with my 512Kb of memory I can Remember ANYTHING you desire, I am at your service, just push /remind"
 bot.owner = "@Mamiglia @ferraririccardo"
 
 r = redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=True)
 
 
-def final_question(cht, t):
+def final_question(cht, t, pretty):
     r.hset(cht.id, 'time', t)
     r.hincrby(cht.id, 'stage')
     bt = botogram.Buttons()
     bt[0].callback('Confirm', 'confirm')
     bt[0].callback('Cancel', 'cancel')
-    cht.send('Do you really want me to remind you this?', attach=bt)
+    cht.send('Do you really want me to remind you this %s?' % (pretty), attach=bt)
 
 
 @bot.command('start')
@@ -37,17 +36,30 @@ def start(chat, message):
             choose_continent(chat)
 
 
+@bot.command('list')
+def list(chat, message):
+    if chat.type == 'private':
+        d.execute('SELECT * FROM remind WHERE userid=?', (chat.id, ))
+
+
 @bot.command('cancel')
 def cancel_remind(chat, message):
     if chat.type == 'private':
-        chat.send('Remind Canceled')
-        r.delete(chat.id)
+        if r.delete(chat.id) == 1:
+            chat.send('Remind Canceled')
+        else:
+            chat.send('No remind to cancel :c')
 
 
 @bot.command("remind")
 def reminder_start(chat, args, message):
     """ This command allow you to remind something new """
     if chat.type == 'private':
+        d.execute('SELECT timezone FROM users WHERE userid=?', (chat.id, ))
+        if d.fetchone() is None:
+            chat.send('You still haven\'t set timezone and DST, press /start!')
+            return
+        # check if timezone is set
         bt = botogram.Buttons()
         bt[0].callback('Cancel the Remind', 'cancel')
         if r.hsetnx(chat.id, 'stage', 1) == 1:
@@ -69,31 +81,38 @@ def stager(chat, message):
             timezone_set(chat, message)
             return
         if stage == 1:
+            bot.edit_message(message.message_id - 1, "Now, Send me the task that you need to remember")
             r.hset(chat.id, 'mesid', message.id)
             bt = botogram.Buttons()
-            bt[0].callback('Cancel', 'cancel')
             bt[1].callback('1 Day', 'timeadd', str(1440))
             bt[1].callback('1 Hour', 'timeadd', str(60))
             bt[1].callback('10 min', 'timeadd', str(10))
             bt[1].callback('5 min', 'timeadd', str(5))
-            # TODO add support to dates composed by only minutes
-            chat.send("When are you interested in remembering this?\nYou can send me date in this format DD/MM/YY or just the minutes", attach=bt)
+            chat.send("When are you interested in remembering this?\nYou can send me in how many minutes or the date in any format", attach=bt)
             r.hincrby(chat.id, 'stage')
             # TODO delete old buttons here and below
         elif stage == 2:
             text = message.text
-            try:
-                t = parser.parse(text)
-            except ValueError:
-                chat.send('Invalid input')
-                return
-            d.execute('SELECT DST, timezone FROM users WHERE userid=?', (chat.id, ))
-            tz = d.fetchone()
-            t = t - timedelta(minutes=(tz[0]+tz[1]))
+            if len(text) < 4:
+                t = datetime.utcnow() + timedelta(minutes=(int(text)))
+                pretty = "in %s minutes" % (text)
+            else:
+                try:
+                    t = parser.parse(text)
+                except ValueError:
+                    chat.send('Retry')
+                    bot.edit_message(message.message_id - 1, "Invalid Input, retry")
+                    return
+                pretty = t.strftime("on %A %d/%m/%y at %H:%M")
+                d.execute('SELECT DST, timezone FROM users WHERE userid=?', (chat.id, ))
+                tz = d.fetchone()
+                t = t - timedelta(minutes=(tz[0]+tz[1]))
             if t > datetime.utcnow():
-                final_question(chat, t)
+                final_question(chat, t, pretty)
+                bot.edit_message(message.message_id - 1, "Ok, you selected %s" % (pretty))
             else:
                 chat.send('I can\'t send messages in the past, I\'m not enough powerful\nChoose another date')
+                bot.edit_message(message.message_id - 1, "Invalid Input, retry")
 
 
 @bot.callback('cancel')
@@ -115,7 +134,8 @@ def confirm(chat, message):
 def timeadd(chat, message, data):
     message.edit(message.text)
     t = datetime.now() + timedelta(minutes=int(data))
-    final_question(chat, t)
+    pretty = "in %s minutes" % (data)
+    final_question(chat, t, pretty)
 
 
 @bot.callback('choose_continent')
@@ -188,7 +208,7 @@ def check_rem():
     d.execute('SELECT * FROM remind')
     reminds = d.fetchall()
     for z in reminds:
-        if datetime.strptime(z[2], "%Y-%m-%d %H:%M:%S") <= datetime.utcnow():
+        if datetime.strptime(z[2], "%Y-%m-%d %H:%M:%S.%f") <= datetime.utcnow():
             bot.api.call("forwardMessage", {"chat_id": z[0], "message_id": z[1], "from_chat_id": z[0]})
             d.execute('DELETE FROM remind WHERE userid=? AND mesid=?', (z[0], z[1]))
             dat.commit()
